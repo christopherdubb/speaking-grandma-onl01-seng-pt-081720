46e5496e21634ff233eb56314d526a76c5ee6c6f
astute-macro-4903 <christopher.williams5699@gmail.com>
2020-08-24 23:46:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,11 +14,11 @@
 def speak_to_grandma(string)
   
   if string.upcase != string
-   return "HUH?! SPEAK UP, SONNY"
-  elsif string == "I LOVE YOU GRANDMA"
-  return "I LOVE YOU TOO PUMPKIN"
+   return "HUH?! SPEAK UP, SONNY!"
+  elsif string == "I LOVE YOU GRANDMA!"
+  return "I LOVE YOU TOO PUMPKIN!"
 else 
-   return "NO, NOT SINCE 1938"
+   return "NO, NOT SINCE 1938!"
 end
 
 end
