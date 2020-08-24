294cb4cd23a0c5ed2c9dda18955c83b32af5657e
astute-macro-4903 <christopher.williams5699@gmail.com>
2020-08-24 23:37:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,7 +11,8 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
-def speak_to_grandma(string) 
+def speak_to_grandma(string)
+  
   if string.upcase == string
     puts "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
@@ -19,4 +20,5 @@ def speak_to_grandma(string)
 else 
   puts "HUH?! SPEAK UP, SONNY!"
 end
+
 end
